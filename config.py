fc0333fe77135ad777f4c1cb8add30b107769c7b
lzborges1 <lborges1@babson.edu>
2023-11-20 17:20:43 -0500

    Project

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAPBOX_TOKEN = "pk.eyJ1IjoibGJvcmdlczEiLCJhIjoiY2xvdDVvMmhjMDZuMTJpbno3cm9sYWpkdCJ9.UKz9m-FRRm8mGTAbYUo00Q" 
+TRAFFIC_API_KEY = "LXGJU837T6rIksKmQQNwbl7m7GabzgpZ"
