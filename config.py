1ca436ce8b27329a4cdb97fddab27cd082f72212
lzborges1 <lborges1@babson.edu>
2023-11-20 19:33:53 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ load_dotenv()
 
 MAPBOX_TOKEN = "pk.eyJ1IjoibGJvcmdlczEiLCJhIjoiY2xvdDVvMmhjMDZuMTJpbno3cm9sYWpkdCJ9.UKz9m-FRRm8mGTAbYUo00Q" 
 TRAFFIC_API_KEY = "LXGJU837T6rIksKmQQNwbl7m7GabzgpZ"
+DIFF_KEY = ""
 
