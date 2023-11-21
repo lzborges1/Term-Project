6a5585388c4125643b2ef382b2fc13837a8c0fd1
lzborges1 <lborges1@babson.edu>
2023-11-20 19:11:55 -0500

    update

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MAPBOX_TOKEN = "pk.eyJ1IjoibGJvcmdlczEiLCJhIjoiY2xvdDVvMmhjMDZuMTJpbno3cm9sYWpkdCJ9.UKz9m-FRRm8mGTAbYUo00Q" 
-TRAFFIC_API_KEY = "LXGJU837T6rIksKmQQNwbl7m7GabzgpZ"
+TRAFFIC_INCIDENT_API_KEY = "M1v6X9ZsPBshhktuGqBKzWNcDaGUVHIE"
+SPEED_KEY = ""
