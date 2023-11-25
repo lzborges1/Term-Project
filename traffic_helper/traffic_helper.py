7cf8a9d7a5a704f0645877896e562d9b51442904
lzborges1 <lborges1@babson.edu>
2023-11-25 16:01:20 -0500

    Update traffic_helper.py

--- traffic_helper/traffic_helper.py
+++ traffic_helper/traffic_helper.py
@@ -1,58 +1,54 @@
+# traffic_helper.py
 import requests
-import json
-from config import MAPBOX_TOKEN, TRAFFIC_INCIDENT_API_KEY
+from config import MAPBOX_TOKEN, TOMTOM_API_KEY
 
-MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places" # To create and display data on a map
-TRAFFIC_INCIDENT_BASE_URL = "https://www.mapquestapi.com/traffic/v2/incidents" # Traffic Incidents
-TOM_TRAFFIC_DATA_BASE_URL = "https://api.tomtom.com/traffic/services/4/incidentViewport/-939584.4813015489,-23954526.723651607,14675583.153020501,25043442.895825107/2/-939584.4813015489,-23954526.723651607,14675583.153020501,25043442.895825107/2/true/xml?key={Traffic_API_Key}"
+MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
+TOM_TRAFFIC_DATA_BASE_URL = "https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json"
 
 def get_traffic_data(api_endpoint, params):
-    # Your code to fetch data from an API
     response = requests.get(api_endpoint, params=params)
-    # Check if the request was successful
     if response.status_code == 200:
-        return response.json()  # Return the JSON response if successful
+        return response.json()
     else:
         response.raise_for_status()
 
 def process_raw_data(raw_data):
-    # Your code to process raw data
     processed_data = []
-    for item in raw_data['trafficItems']:
-        # Process each item (this is just an example and will depend on the actual structure of your data)
+    # Adjust according to the actual structure of the TomTom API response
+    for item in raw_data['flowSegmentData']:
         processed_data.append({
-            'flow': item['flow'],
-            'coordinates': item['location']['geographic'],
-            'severity': item['severity']
+            'currentSpeed': item.get('currentSpeed', 'Not Available'),
+            'freeFlowSpeed': item.get('freeFlowSpeed', 'Not Available'),
+            'currentTravelTime': item.get('currentTravelTime', 'Not Available'),
+            'freeFlowTravelTime': item.get('freeFlowTravelTime', 'Not Available'),
         })
     return processed_data
 
 def calculate_traffic_flow(data):
-    # Your code to calculate traffic flow
-    total_flow = sum(item['flow'] for item in data if 'flow' in item)
-    # Calculate average flow if needed, or return the total
-    average_flow = total_flow / len(data) if data else 0
-    return average_flow
+    total_speed = sum(item['currentSpeed'] for item in data if 'currentSpeed' in item)
+    average_speed = total_speed / len(data) if data else 0
+    return average_speed
 
-# ... other function definitions ...
-
-# Example of a function that uses the above functions
 def analyze_traffic_for_route(route_coordinates):
-    raw_data = get_traffic_data(api_endpoint='http://api.traffic.com/data', params={'coordinates': route_coordinates})
+    params = {
+        'key': TOMTOM_API_KEY,
+        'point': route_coordinates,
+        # Add additional parameters as required by the API
+    }
+    raw_data = get_traffic_data(TOM_TRAFFIC_DATA_BASE_URL, params)
     processed_data = process_raw_data(raw_data)
     traffic_flow = calculate_traffic_flow(processed_data)
-    # Maybe more analysis here
     return traffic_flow
 
-# Additional utility functions as needed
 def log_activity(message):
-    # Your code to log activity or errors
-    pass
+    
+    print(message)
 
 def main():
-    """
-    
-    """
+    # Example coordinates for a location on I-95
+    route_coordinates = '25.782545,-80.299676'
+    traffic_info = analyze_traffic_for_route(route_coordinates)
+    print(traffic_info)
 
 if __name__ == '__main__':
     main()
