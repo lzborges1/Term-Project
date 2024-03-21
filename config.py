0b688459969daba10cdbd99e68c2c8a06bd649b8
lzborges1 <lborges1@babson.edu>
2024-03-21 12:55:37 -0400

    Fix

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MAPBOX_TOKEN = "pk.eyJ1IjoibGJvcmdlczEiLCJhIjoiY2xwdW84em0yMGx0cTJqcnBzaWp3YWttNiJ9.O2oKgiYAdUgZXnnTMR676Q" 
-TRAFFIC_API_KEY = "LXGJU837T6rIksKmQQNwbl7m7GabzgpZ"
+#MAPBOX_TOKEN = "pk.eyJ1IjoibGJvcmdlczEiLCJhIjoiY2xwdW84em0yMGx0cTJqcnBzaWp3YWttNiJ9.O2oKgiYAdUgZXnnTMR676Q" 
+#TRAFFIC_API_KEY = "LXGJU837T6rIksKmQQNwbl7m7GabzgpZ"
